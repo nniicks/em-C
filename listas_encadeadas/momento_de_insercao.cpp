be2678d9c5da45430e69d62c261a2e5f90cc3cf7
nniicks <jhonathanduartebarreto@gmail.com>
2023-10-31 21:59:33 -0400

    busca e posicao de insercao

--- listas_encadeadas/momento_de_insercao.cpp
+++ listas_encadeadas/momento_de_insercao.cpp
@@ -1,56 +1,61 @@
+/*bunscando em uma lista. os elementos foram
+todos inseridos no comeco da lista*/
 #include <stdio.h>
 #include <stdlib.h>
 
 struct celula{
     int chave;
+    int posicao;
     struct celula *proximo;
 };
 
-void leitura(struct celula* &lst, int numero);
+void leitura(int numero, struct celula* &lst, int posicao);
 
 void imprime(struct celula *lst);
 
-celula *busca(int numero, struct celula *lst, int *posicao);
+celula *busca(int numero, struct celula *lst);
 
 int main(){
     struct celula *lista = NULL, *p;
     int numero, aux = 0;
     
-    printf("Entre com um numero inteiro:\n");
+    printf("Entre com um valor inteiro:\n");
     scanf("%d", &numero);
     
     while(numero != -1){
-        leitura(lista, numero);
+        leitura(numero, lista, aux);
+        aux++;
         
-        printf("Entre com um inteiro:\n");
+        printf("Entre com um valor inteiro:\n");
         scanf("%d", &numero);
     }
     
-    printf("Digite um numero qualquer ai:\n");
+    printf("Digite um inteiro ai:\n");
     scanf("%d", &numero);
     
-    p = busca(numero, lista, &aux);
-    
-    printf("Lista como um todo:\n");
-    imprime(lista);
+    p = busca(numero, lista);
     if(p == NULL){
-        printf("\nO numero %d nao foi encontrado na lista!\n", numero);
+        printf("O numero %d nao foi encontrado na lsita!\n", numero);
     }else{
-        printf("\nO numero %d foi encontrado na posicao %d da lista!\n", numero, aux);
+        printf("O numero %d esta na lista, e ele foi o %d° numero inserido na lista", p->chave, p->posicao);
     }
     
+    printf("\n\nElementos da lista:\n");
+    imprime(lista);
+    
     return 0;
+    
 }
 
-void leitura(struct celula* &lst, int numero){
+void leitura(int numero, struct celula* &lst, int posicao){
     struct celula *nova;
     
     nova = (struct celula *) malloc(sizeof(struct celula));
     
     nova->chave = numero;
+    nova->posicao = posicao;
     nova->proximo = lst;
     lst = nova;
-    
 }
 
 void imprime(struct celula *lst){
@@ -61,15 +66,13 @@ void imprime(struct celula *lst){
     }
 }
 
-struct celula *busca(int numero, struct celula *lst, int *posicao){
+celula *busca(int numero, struct celula *lst){
     struct celula *p;
     
     p = lst;
     while(p != NULL && p->chave != numero){
         p = p->proximo;
-        (*posicao)++;
-        
     }
-   
+    
     return p;
 }
