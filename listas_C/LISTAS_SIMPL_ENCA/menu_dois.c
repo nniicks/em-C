d9bc26c1c88d2f254e2ed3025b142971d0c479ad
nniicks <jhonathanduartebarreto@gmail.com>
2024-01-09 22:10:35 -0400

    lista 2.0

--- listas_C/LISTAS_SIMPL_ENCA/menu_dois.c
+++ listas_C/LISTAS_SIMPL_ENCA/menu_dois.c
@@ -1,5 +1,3 @@
-//INCLUIR DEOOIS A FUNCAO QUE INSERE ANTES DA CHAVE
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,18 +6,26 @@ typedef struct cel{
     struct cel *proximo;
 }celula;
 
-void imprime(celula *lista);
+typedef struct{
+    celula *inicio;
+    int tam;
+}Lista;
 
-void insere_inicio(celula **lista, int numero);
+void criar_lista(Lista *lista);
 
-void insere_fim(celula **lista, int numero);
+void imprime(Lista lista);
 
-void insere_apos_chave(celula **lista, int numero, int chave);
+void insere_inicio(Lista *lista, int numero);
+
+void insere_fim(Lista *lista, int numero);
+
+void insere_apos_chave(Lista *lista, int numero, int chave);
 
 int main(){
-    celula *lista = NULL;
+    Lista lista;
     int opc, numero, chave;
     
+    criar_lista(&lista);
     do{
         printf("\n\n0 - SAIR\n1 - IMPRIME\n2 - INSERE NO INICIO\n3 - INSERE NO FIM\n4 - INSERE APOS A CHAVE\n\n");
         scanf("%d", &opc);
@@ -48,66 +54,76 @@ int main(){
     
 }
 
-void imprime(celula *lista){
-    printf("\n------ LISTA -------\n");
-    while(lista != NULL){
-        printf("%d\n", lista->chave);
-        lista = lista->proximo;
+void criar_lista(Lista *lista){
+    lista->inicio = NULL;
+    lista->tam = 0;
+}
+
+void imprime(Lista lista){
+    celula *aux = lista.inicio;
+    
+    printf("\n------ LISTA: %d -------\n", lista.tam);
+    while(aux != NULL){
+        printf("%d\n", aux->chave);
+        aux = aux->proximo;
     }
     printf("\n------ FIM LISTA -------\n");
 }
 
-void insere_inicio(celula **lista, int numero){
+void insere_inicio(Lista *lista, int numero){
     celula *nova = malloc(sizeof(celula));
     
     if(nova !=  NULL){
         nova->chave = numero;
-        nova->proximo = *lista;
-        *lista = nova;
+        nova->proximo = lista->inicio; //o proximo da nova celula tem que apontar para o incio da lista
+        lista->inicio = nova;  //atualiza o inicio
+        lista->tam++;
     }else{
         printf("Erro na alocaçao de memoria!\n");
     }
 }
 
-void insere_fim(celula **lista, int numero){
+void insere_fim(Lista *lista, int numero){
     celula *aux, *nova = malloc(sizeof(celula));
     
     if(nova != NULL){
         nova->chave = numero;
-        nova->proximo = NULL;
+        nova->proximo = NULL; //como a inserçao eh sempre no final, o proximo sempre sera NULL
         
-        if(*lista == NULL){
-            *lista = nova;
+        if(lista->inicio == NULL){//lista esta vazia?
+            lista->inicio = nova;
         }else{
-            aux = *lista;  //faz aux apontar para o inicio da lista
+            aux = lista->inicio;  //faz aux apontar para o inicio da lista
             while(aux->proximo != NULL){
                 aux = aux->proximo;
             }
             aux->proximo = nova;
         }
+        lista->tam++;
     }else{
         printf("Erro ao alocar memoria!\n");
     }
 }
 
-void insere_apos_chave(celula **lista, int numero, int chave){
+void insere_apos_chave(Lista *lista, int numero, int chave){
     celula *aux, *nova = malloc(sizeof(celula));
     
     if(nova != NULL){
         nova->chave = numero;
         //caso seja o primeiro:
-        if(*lista == NULL){
+        if(lista->inicio == NULL){
             nova->proximo = NULL;
-            *lista = nova;
+            lista->inicio = nova;  //atualiza o inicio da lista
         }else{
             
-            aux = *lista;
+            aux = lista->inicio;
             while(aux->chave != chave && aux->proximo != NULL){
                 aux = aux->proximo;
             }
             nova->proximo = aux->proximo;
             aux->proximo = nova;
         }
+        lista->tam++;
     }else{
         printf("Erro ao alocar memoria!\n");
     }
